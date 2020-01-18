82cfe9cedb22ab344c3468bbecf74eab86150fae
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-01-18 20:00:50 -0300

    footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2020
+            <a href='https://www.instagram.com/paulo_germanopg/' target='_blank'> Instagram</a>.
+        </strong>
+    </footer>
+)
