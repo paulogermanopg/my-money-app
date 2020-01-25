df9b3c4a9af98945ec54982d3d74633e0bdab922
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-01-25 10:42:00 -0200

    mensagens de erro e sucs

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,6 +4,7 @@ import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
 import Routes from './routes'
+import Messages from '../common/msg/messages'
 
 
 export default props => (
@@ -14,5 +15,6 @@ export default props => (
             <Routes />
         </div>
         <Footer />
+        <Messages />
     </div>
 )
