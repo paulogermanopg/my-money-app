967588ba28a468f7107b2badc09952f2507e90ce
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-01-27 20:39:36 -0200

    ações

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -12,7 +12,6 @@ class BillingCycleList extends Component {
 
     renderRows() {
         const list = this.props.list || []
-        console.log(this.props.list)
         return list.map(bc => (
             <tr key={bc._id}>
                 <td>{bc.name}</td>
